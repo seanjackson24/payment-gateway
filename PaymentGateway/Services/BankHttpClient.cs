0410302ecaeef0557e69a85694a16a06588d0374
Sean Jackson <sean.jackson24@gmail.com>
2020-09-09 12:33:59 +0100

    tidy up

--- PaymentGateway/Services/BankHttpClient.cs
+++ PaymentGateway/Services/BankHttpClient.cs
@@ -3,16 +3,19 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace PaymentGateway.Services
 {
 	public class TestBankHttpClient
 	{
 		private readonly HttpClient _httpClient;
+		private readonly IConfiguration _configuration;
 
-		public TestBankHttpClient(HttpClient client)
+		public TestBankHttpClient(HttpClient client, IConfiguration configuration)
 		{
 			_httpClient = client;
+			_configuration = configuration;
 		}
 
 		public async Task<TestBankPaymentResponse> RequestBankPayment(TestBankPaymentRequest request, CancellationToken cancellationToken)
@@ -21,7 +24,7 @@ namespace PaymentGateway.Services
 			{
 				throw new ArgumentNullException(nameof(request));
 			}
-			string url = "TODO";
+			string url = _configuration.GetValue<string>("BankSimulator.Url");
 			var httpRequest = new HttpRequestMessage(HttpMethod.Get, url);
 			var response = await _httpClient.SendAsync(httpRequest, HttpCompletionOption.ResponseHeadersRead, cancellationToken);
 
